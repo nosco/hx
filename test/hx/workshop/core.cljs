e8aacc79a2971e688beea83dd6fd225949ddd52f
Will Acton <will@willacton.com>
2018-06-16 09:17:24 -0700

    change "react" alias to "hx" in workshop

--- test/hx/workshop/core.cljs
+++ test/hx/workshop/core.cljs
@@ -1,102 +1,103 @@
 (ns hx.workshop.core
   (:require [devcards.core :as dc :include-macros true]
-            [hx.react :as react :include-macros true]))
+            [hx.react :as hx :include-macros true]
+            [hx.state]))
 
 (dc/defcard
   macroexpand
-  (macroexpand '(react/compile
-                $[:div {:style {:color "green"}
-                        :id "asdf"} "hello"])))
+  (macroexpand '(hx/compile
+                 $ [:div {:style {:color "green"}
+                          :id "asdf"} "hello"])))
 
 (dc/defcard
   simple
-  (react/compile
-   $[:div {:style {:color "green"}
-          :id "asdf"} "hello"]))
+  (hx/compile
+   $ [:div {:style {:color "green"}
+            :id "asdf"} "hello"]))
 
 (dc/defcard
   with-children
-  (react/compile
-   $[:ul {:style {:background "lightgrey"}}
-     [:li {:style {:font-weight "bold"}} "one"]
-     [:li "two"]
-     [:li "three"]]))
+  (hx/compile
+   $ [:ul {:style {:background "lightgrey"}}
+      [:li {:style {:font-weight "bold"}} "one"]
+      [:li "two"]
+      [:li "three"]]))
 
 (dc/defcard
   conditional
-  (react/compile
-   $[:<>
-     (when true
-       $[:div "true"])
-     (when false
-       $[:div "false"])]))
+  (hx/compile
+   $ [:<>
+      (when true
+        $ [:div "true"])
+      (when false
+        $ [:div "false"])]))
 
 (dc/defcard
   seq
-  (react/compile
-   $[:ul
-     (list $[:li {:key 1} 1]
-           $[:li {:key 2} 2])]))
+  (hx/compile
+   $ [:ul
+      (list $ [:li {:key 1} 1]
+            $ [:li {:key 2} 2])]))
 
 (dc/defcard
   map
-  (react/compile
+  (hx/compile
    (let [numbers [1 2 3 4 5]]
-     $[:ul {:style {:list-style-type "square"}}
-       (map #(do $[:li {:key %} %])
-            numbers)])))
+     $ [:ul {:style {:list-style-type "square"}}
+        (map #(do $ [:li {:key %} %])
+             numbers)])))
 
 (dc/defcard css-class
-  (react/compile
-   $[:<>
-     [:style {:dangerouslySetInnerHTML #js {:__html ".foo { color: lightblue }"}}]
-     [:div {:className "foo"} "asdf jkl"]
-     [:div {:class "foo"} "1234 bnm,"]]))
+  (hx/compile
+   $ [:<>
+      [:style {:dangerouslySetInnerHTML #js {:__html ".foo { color: lightblue }"}}]
+      [:div {:className "foo"} "asdf jkl"]
+      [:div {:class "foo"} "1234 bnm,"]]))
 
 (dc/defcard defnc
-  (macroexpand '(react/defnc greeting [{:keys [name] :as props}]
+  (macroexpand '(hx/defnc greeting [{:keys [name] :as props}]
                   (println props)
-                  $[:span {:style {:font-size "24px"}}
-                    "Hello, " name])))
+                  $ [:span {:style {:font-size "24px"}}
+                     "Hello, " name])))
 
-(react/defnc greeting [{:keys [name] :as props}]
-  $[:span {:style {:font-size "24px"}}
-    "Hello, " name])
+(hx/defnc greeting [{:keys [name] :as props}]
+  $ [:span {:style {:font-size "24px"}}
+     "Hello, " name])
 
 (dc/defcard
   function-element
-  (react/compile
-   $[greeting {:name "Will"}]))
+  (hx/compile
+   $ [greeting {:name "Will"}]))
 
-(react/defnc with-children [{:keys [children]}]
-  $[:div
-    (identity children)])
+(hx/defnc with-children [{:keys [children]}]
+  $ [:div
+     (identity children)])
 
 (dc/defcard with-children
-  (react/compile
-   $[with-children
-     [:span "hi"]
-     [:div "watup"]]))
+  (hx/compile
+   $ [with-children
+      [:span "hi"]
+      [:div "watup"]]))
 
 (dc/defcard defcomponent
-  (macroexpand '(react/defcomponent some-component
+  (macroexpand '(hx/defcomponent some-component
                   (constructor [this]
                                this)
                   (render [this]
-                          $[:div "sup component"]))))
+                          $ [:div "sup component"]))))
 
-(react/defcomponent
+(hx/defcomponent
   some-component
   (constructor [this]
                this)
   (render [this]
-          $[:div "sup component"]))
+          $ [:div "sup component"]))
 
 (dc/defcard class-element
-  (react/compile
-   $[some-component]))
+  (hx/compile
+   $ [some-component]))
 
-(react/defcomponent stateful
+(hx/defcomponent stateful
   (constructor [this]
                (set! (.. this -state) #js {:name "Will"})
                this)
@@ -104,16 +105,16 @@
                 (. this setState #js {:name (.. e -target -value)}))
   (render [this]
           (let [state (. this -state)]
-            $[:div
-              [:div (. state -name)]
-              [:input {:value (. state -name)
-                       :on-change (. this -update-name!)}]])))
+            $ [:div
+               [:div (. state -name)]
+               [:input {:value (. state -name)
+                        :on-change (. this -update-name!)}]])))
 
 (dc/defcard stateful-element
-  (react/compile
-   $[stateful]))
+  (hx/compile
+   $ [stateful]))
 
-(react/defcomponent static-property
+(hx/defcomponent static-property
   (constructor [this]
                this)
 
@@ -121,13 +122,13 @@
   (some-prop "1234")
 
   (render [this]
-          $[:div (. static-property -some-prop)]))
+          $ [:div (. static-property -some-prop)]))
 
 (dc/defcard static-property
-  (react/compile
-   $[static-property]))
+  (hx/compile
+   $ [static-property]))
 
-(react/defcomponent fn-as-child
+(hx/defcomponent fn-as-child
   (constructor [this]
                (set! (. this -state) #js {:name "Will"})
                this)
@@ -135,18 +136,18 @@
                 (. this setState #js {:name (.. e -target -value)}))
   (render [this]
           (let [state (. this -state)]
-            $[:div
-              [:div ((.. this -props -children) (. state -name))]
-              [:input {:value (. state -name)
-                       :on-change (. this -update-name!)}]])))
+            $ [:div
+               [:div ((.. this -props -children) (. state -name))]
+               [:input {:value (. state -name)
+                        :on-change (. this -update-name!)}]])))
 
 (dc/defcard fn-as-child
-  (react/compile
-   $[fn-as-child
-     (fn [name]
-       $[:span {:style {:color "red"}} name])]))
+  (hx/compile
+   $ [fn-as-child
+      (fn [name]
+        $ [:span {:style {:color "red"}} name])]))
 
-(react/defcomponent render-prop
+(hx/defcomponent render-prop
   (constructor [this]
                (set! (. this -state) #js {:name "Will"})
                this)
@@ -154,28 +155,25 @@
                 (. this setState #js {:name (.. e -target -value)}))
   (render [this]
           (let [state (. this -state)]
-            $[:div
-              [:div ((.. this -props -render) (. state -name))]
-              [:input {:value (. state -name)
-                       :on-change (. this -update-name!)}]])))
+            $ [:div
+               [:div ((.. this -props -render) (. state -name))]
+               [:input {:value (. state -name)
+                        :on-change (. this -update-name!)}]])))
 
 (dc/defcard render-prop
-  (react/compile
-   $[render-prop
-     {:render (fn [name]
-                $[:span {:style {:color "red"}} name])}]))
+  (hx/compile
+   $ [render-prop
+      {:render (fn [name]
+                 $ [:span {:style {:color "red"}} name])}]))
 
 (def js-interop-test
   (fn
     [props]
-    (js/console.log props)
-    "blahblah"))
-
-(js/console.log js-interop-test)
+    (js/JSON.stringify props)))
 
 (dc/defcard js-interop-nested-props
-  (react/compile
-   $[js-interop-test {:nested {:thing {:foo {:bar "baz"}}}}]))
+  (hx/compile
+   $ [js-interop-test {:nested {:thing {:foo {:bar "baz"}}}}]))
 
 (defn ^:dev/after-load start! []
   (dc/start-devcard-ui!))
