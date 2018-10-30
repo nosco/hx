4df86b6ed1880b6cad9e80e3d35635bd495e0716
Will Acton <will@willacton.com>
2018-10-30 14:24:53 -0700

    cleaning out my closet

--- src/hx/react/dom.cljs
+++
@@ -1,9 +0,0 @@
-(ns hx.react.dom
-  (:refer-clojure :exclude [map meta time])
-  (:require [hx.react :as r]
-            [react :as react])
-  (:require-macros [hx.react.dom]))
-
-(hx.react.dom/make-factories)
-
-(def <> (r/factory react/Fragment))
