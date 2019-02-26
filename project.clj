74b943382279a1dacb85060c4326a085c3d82448
Will Acton <will@willacton.com>
2019-02-25 21:18:22 -0800

    bump project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lilactown/hx "0.3.1"
+(defproject lilactown/hx "0.3.2"
   :description "An easy to use, decomplected hiccup compiler for ClojureScript & React."
   :url "https://github.com/Lokeh/hx"
   :license {:name "Eclipse Public License"
