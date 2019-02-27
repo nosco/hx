f3e16cb4dad38b3fce319fa8db6c8b2096c12ef4
Will <will@willacton.com>
2019-02-26 21:02:53 -0800

    Update material.cljs

--- examples/workshop/material.cljs
+++ examples/workshop/material.cljs
@@ -1,35 +1,41 @@
 (ns workshop.material
   (:require [devcards.core :as dc :include-macros true]
-            [hx.react :as hx]
+            [hx.react :as hx :refer [defnc]]
             ["@material-ui/core/Button" :default Button]
             ["@material-ui/core/CircularProgress" :default CircularProgress]))
 
-(hx/defnc MuStyles [_]
-  [:link {:rel "stylesheet"
-          :href "https://fonts.googleapis.com/css?family=Roboto:300,400,500"}])
-
 (dc/defcard adding-styles*
-  (hx/f [MuStyles]))
+  ;; add the Robot font globally on the page
+  (hx/f [:link {:rel "stylesheet"
+                :href "https://fonts.googleapis.com/css?family=Roboto:300,400,500"}]))
+
+(defnc ButtonExample [_]
+  [:<>
+   (for [variant [nil "outlined" "contained" "fab" "extendedFab"]]
+     [:div (or variant "default")
+      (for [color [nil "primary" "secondary"]]
+        [Button {:variant variant :color color
+                 :style {:margin "5px"}}
+         (or color "default")])])]
 
 (dc/defcard button
-  (hx/f [:<>
-         (for [variant [nil "outlined" "contained" "fab" "extendedFab"]]
-           [:div (or variant "default")
-            (for [color [nil "primary" "secondary"]]
-              [Button {:variant variant :color color
-                       :style {:margin "5px"}}
-               (or color "default")])])]))
+  ;; hx/f is used here to instantiate the `ButtonExample` component as a React element
+  ;; to work with devcards
+  (hx/f [ButtonExample]))
+  
+(defnc CircularProgressExample [_]
+  [:<>
+   [:div "Indeterminate "
+    [:div {:style {:padding "5px"}}
+     [CircularProgress {:color "primary"}]]]
+   [:div "Determinate"
+    [:div {:style {:padding "5px"}}
+     [CircularProgress {:color "primary" :value 33
+                        :variant "determinate"}]]]
+   [:div "Static"
+    [:div {:style {:padding "5px"}}
+     [CircularProgress {:color "primary" :value 33
+                        :variant "static"}]]]]
 
 (dc/defcard circular-progress
-  (hx/f [:<>
-         [:div "Indeterminate "
-          [:div {:style {:padding "5px"}}
-           [CircularProgress {:color "primary"}]]]
-         [:div "Determinate"
-          [:div {:style {:padding "5px"}}
-           [CircularProgress {:color "primary" :value 33
-                              :variant "determinate"}]]]
-         [:div "Static"
-          [:div {:style {:padding "5px"}}
-           [CircularProgress {:color "primary" :value 33
-                              :variant "static"}]]]]))
+  (hx/f [CircularProgressExample]))
