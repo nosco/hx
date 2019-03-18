c1c341cc766c9b431057e0d82c98a88fe85eaf36
Will Acton <will@willacton.com>
2019-03-18 11:35:20 -0700

    remove pret

--- test/hx/hooks_test.cljs
+++ test/hx/hooks_test.cljs
@@ -172,7 +172,7 @@
                        (u/click)
                        (u/click))]
     (t/is (u/node= (u/html "<div>3</div>")
-                   (u/pret state-test)))))
+                   state-test))))
 
 (hx/defnc StateWithEffect [{:keys [receive]}]
   (let [[count update-count] (hooks/<-state 0)]
