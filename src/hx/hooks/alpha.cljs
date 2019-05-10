b998e4cd926fb943dac1c700f378c22f34f5aa7b
Will Acton <will@willacton.com>
2019-05-10 15:27:35 -0700

    useReducerOnce, persist state when not in debug mode for consistency

--- src/hx/hooks/alpha.cljs
+++ src/hx/hooks/alpha.cljs
@@ -1,38 +1,59 @@
 (ns hx.hooks.alpha
-  (:require [hx.hooks :refer [useEffect useState useIRef]])
+  (:require [hx.hooks :refer [useEffect useState useReducer useIRef]])
   (:require-macros [hx.hooks.alpha]))
 
 (defonce states (atom {}))
 
-(defn useReloadable
+(defn usePersistentHook
   [use-hook k & {:keys [initial derive]
                  :or {initial nil
                       derive identity}}]
-  (if js/goog.DEBUG
-    ;; if a state already exists with name `k`, then pass that value in
-    ;; otherwise, pass in the initial value.
-    ;; capture the hook returned by the higher-order hook passed in
-    (let [hook (use-hook (if (contains? @states k)
-                           (@states k)
-                           initial))]
-      (let [has-mounted? (useIRef false)]
-        (useEffect (fn []
-                     (if @has-mounted?
-                       (swap! states assoc k (derive hook))
-                       (reset! has-mounted? true)))
-                   [(derive hook)]))
-      hook)
-    ;; in release mode, just return <-state
-    (use-hook initial)))
+  ;; if a state already exists with name `k`, then pass that value in
+  ;; otherwise, pass in the initial value.
+  ;; capture the hook returned by the higher-order hook passed in
+  (let [hook (use-hook (if (contains? @states k)
+                         (@states k)
+                         initial))]
+    (let [has-mounted? (useIRef false)]
+      (useEffect (fn []
+                   (if @has-mounted?
+                     (swap! states assoc k (derive hook))
+                     (reset! has-mounted? true)))
+                 [(derive hook)]))
+    hook)
+  ;; in release mode, just return <-state
+  ;; (use-hook initial)
+  )
 
 (defn useStateOnce
   "Like useState, but maintains your state across hot-reloads. `k` is a globally
   unique key to ensure you always get the same state back.
 
-  Example: `(useStateOnce ::counter 0)`"
+  Example: `(useStateOnce 0 ::counter)`"
   [initial k]
-  (useReloadable
+  (usePersistentHook
    (fn useStateOnce* [state] (useState state))
    k
    :initial initial
    :derive first))
+
+(defn useReducerOnce
+  "Like useReducer, but maintains your state across hot-reloads. `k` is a globally
+  unique key to ensure you always get the same state back.
+
+  Example: `(useReducerOnce reducer initial ::counter)`"
+  [reducer initial k]
+  ;; if a state already exists with name `k`, then pass that value in
+  ;; otherwise, pass in the initial value.
+  ;; capture the hook returned by the higher-order hook passed in
+  (let [hook (useReducer reducer
+                         (if (contains? @states k)
+                           (@states k)
+                           initial))]
+    (let [has-mounted? (useIRef false)]
+      (useEffect (fn []
+                   (if @has-mounted?
+                     (swap! states assoc k (first hook))
+                     (reset! has-mounted? true)))
+                 [(first hook)]))
+    hook))
