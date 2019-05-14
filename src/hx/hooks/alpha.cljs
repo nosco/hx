e79e23985c410fd31f982a669edd3a39931dae06
Will Acton <will@willacton.com>
2019-05-14 08:41:52 -0700

    fix up useStateOnce

--- src/hx/hooks/alpha.cljs
+++ src/hx/hooks/alpha.cljs
@@ -1,29 +1,8 @@
 (ns hx.hooks.alpha
-  (:require [hx.hooks :refer [useEffect useState useReducer useIRef]])
+  (:require [hx.hooks :refer [useEffect useState useReducer useIRef useDebugValue]])
   (:require-macros [hx.hooks.alpha]))
 
 (defonce states (atom {}))
-
-(defn usePersistentHook
-  [use-hook k & {:keys [initial derive]
-                 :or {initial nil
-                      derive identity}}]
-  ;; if a state already exists with name `k`, then pass that value in
-  ;; otherwise, pass in the initial value.
-  ;; capture the hook returned by the higher-order hook passed in
-  (let [hook (use-hook (if (contains? @states k)
-                         (@states k)
-                         initial))]
-    (let [has-mounted? (useIRef false)]
-      (useEffect (fn []
-                   (if @has-mounted?
-                     (swap! states assoc k (derive hook))
-                     (reset! has-mounted? true)))
-                 [(derive hook)]))
-    hook)
-  ;; in release mode, just return <-state
-  ;; (use-hook initial)
-  )
 
 (defn useStateOnce
   "Like useState, but maintains your state across hot-reloads. `k` is a globally
@@ -31,11 +10,21 @@
 
   Example: `(useStateOnce 0 ::counter)`"
   [initial k]
-  (usePersistentHook
-   (fn useStateOnce* [state] (useState state))
-   k
-   :initial initial
-   :derive first))
+  ;; if a state already exists with name `k`, then pass that value in
+  ;; otherwise, pass in the initial value.
+  ;; capture the hook returned by the higher-order hook passed in
+  (let [hook (useState (if (contains? @states k)
+                         (@states k)
+                         initial))
+        has-mounted? (useIRef false)]
+    ;; (useDebugValue (pr-str (first hook)))
+    (useEffect (fn []
+                 (if @has-mounted?
+                   (swap! states assoc k (first hook))
+                   (reset! has-mounted? true)))
+               [(first hook)])
+    hook)
+  )
 
 (defn useReducerOnce
   "Like useReducer, but maintains your state across hot-reloads. `k` is a globally
@@ -51,6 +40,7 @@
                            (@states k)
                            initial))]
     (let [has-mounted? (useIRef false)]
+      ;; (useDebugValue (pr-str (first hook)))
       (useEffect (fn []
                    (if @has-mounted?
                      (swap! states assoc k (first hook))
